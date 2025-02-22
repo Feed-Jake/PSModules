24b8a3a218cd3fa777d26b60e23597096f0b7aad
Jake Daw <jake.daw@gmail.com>
2025-02-22 15:08:31 -0500

    Adding Get-WebText.psM1

---
+++ Get-WebText.psm1
@@ -0,0 +1,20 @@
+function Get-WebText {
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$Url
+    ) 
+    if (-not $PSCmdlet.MyInvocation.BoundParameters.ContainsKey('Url')) {
+        $Url = Read-Host "Please enter the Web URL"
+    }
+    # Fetch the HTML content of the website
+    $response = Invoke-WebRequest -Uri $Url
+
+    # Extract the raw HTML content
+    $htmlContent = $response.Content
+
+    # Use regex to remove HTML tags
+    $textOnlyContent = [regex]::Replace($htmlContent, '<[^>]+>', ' ')
+
+    # Display the text-only content
+    $textOnlyContent
+}
