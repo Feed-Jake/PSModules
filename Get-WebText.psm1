37f4f4e8322e2685d32fa01f86932ad8ab4ea1da
Jake Daw <jakedaw@gmail.com>
2025-02-22 15:32:34 -0500

    adding help file content.

--- Get-WebText.psm1
+++ Get-WebText.psm1
@@ -1,3 +1,19 @@
+<#
+.SYNOPSIS
+    This module is simply to try and gather all of the text content of a Web site. 
+.DESCRIPTION
+    A longer description of the function, its purpose, common use cases, etc.
+.NOTES
+    Information or caveats about the function e.g. 'This function is not supported in Linux'
+.LINK
+    Specify a URI to a help page, this will show when Get-Help -Online is used.
+.EXAMPLE
+    Test-MyTestFunction -Verbose
+    Explanation of the function or its result. You can include multiple examples with additional .EXAMPLE lines
+#>
+
+
+
 function Get-WebText {
     param (
         [Parameter(Mandatory = $true)]
